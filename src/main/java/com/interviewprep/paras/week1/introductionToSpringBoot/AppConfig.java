20efd0711e784c57c269f3e827b3ff1c571f20bb
Paras-Gupta12 <parasgupta3062000@gmail.com>
2024-09-03 16:44:59 +0530

    README file added

--- src/main/java/com/interviewprep/paras/week1/introductionToSpringBoot/AppConfig.java
+++ src/main/java/com/interviewprep/paras/week1/introductionToSpringBoot/AppConfig.java
@@ -1,5 +1,18 @@
 package com.interviewprep.paras.week1.introductionToSpringBoot;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration// you can define multiple type of configuration and also can define some conditional properties like:
+// how and when the configuration should be loaded depending on some other type of configuration
+//
 public class AppConfig {
+
+    @Bean
+    @Scope("prototype") // "singleton" by default
+    Apple getApple(){// factory method
+        return new Apple();// the creation of object part is being created by programmer/developer but the management part/ DI part taken care by Spring boot application
+    }
 
 }
--- src/main/java/com/interviewprep/paras/week1/introductionToSpringBoot/Apple.java
+++ src/main/java/com/interviewprep/paras/week1/introductionToSpringBoot/Apple.java
@@ -1,4 +1,23 @@
 package com.interviewprep.paras.week1.introductionToSpringBoot;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+//@Component
 public class Apple {
+
+    void eatApple(){
+        System.out.println("I am eating the apple");
+    }
+
+    @PostConstruct
+    void callThisBeforeApplicationIsUsed(){
+        System.out.println("Creating the apple before use");
+    }
+
+    @PreDestroy
+    void callThisBeforeDestroy(){
+        System.out.println("Destroying the apple bean");
+    }
 }
